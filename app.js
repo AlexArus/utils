3f5adad87725d9848e8a6f447fdf13ba60acc8b0
AlexArus <mr.alex.arus@gmail.com>
2022-06-03 16:28:14 +0000

    Deploying to gh-pages from @ AlexArus/utils@a30a0b5df6984e6c08cb89a14953ef0a594ebf58 🚀

---
+++ app.js
@@ -0,0 +1,8 @@
+// deno-fmt-ignore-file
+// deno-lint-ignore-file
+// This code was bundled using `deno bundle` and it's not recommended to edit it manually
+
+const appRootEl = document.getElementById('app');
+if (appRootEl) {
+    appRootEl.innerText = 'Application';
+}
